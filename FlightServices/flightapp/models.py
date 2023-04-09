696481106ada7b60e812c452c9329b770beb4f6e
kumar Paras <phenomkps@gmail.com>
2023-04-09 19:40:46 +0530

    adding auto token generation

--- FlightServices/flightapp/models.py
+++ FlightServices/flightapp/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.conf import settings
+
 
 # Create your models here.
 class Flight(models.Model):
@@ -21,3 +26,7 @@ class Reservation(models.Model):
     #one flight can have many reservation
     passenger=models.OneToOneField(Passenger,on_delete=models.CASCADE)
     
+@receiver(post_save,sender=settings.AUTH_USER_MODEL)
+def CreateAuthToken(sender,instance,created,**kwargs):
+    if created:
+        Token.objects.create(user=instance)
